68362d83a58155d5a6b4a0874900deba42f59b28
Ihor Kokotskyi <ihor_kokotskyi@epam.com>
2022-04-22 12:38:42 +0300

    added factorial example

---
+++ week3/factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n < 2:
+        return 1
+    return n*factorial(n-1)
+print("Fatorial of 1 is=",factorial(1))
+print("Factorial of 2 is =", factorial(2))
+print("Factorial of 10 is =", factorial(10))
+
+
+
